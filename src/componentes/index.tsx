1faf9049622b75fa0f0d27e5f9a0b44c4170cca1
Yan-Reule <130118630+Yan-Reule@users.noreply.github.com>
2024-09-04 09:07:52 -0300

    aviso de vitoria

--- src/componentes/index.tsx
+++ src/componentes/index.tsx
@@ -3,39 +3,30 @@ import { useEffect, useState } from "react";
 export default function Home() {
     const [tabuleiro, setTabuleiro] = useState(Array(9).fill(null));
     const [turnoX, setTurnoX] = useState<boolean>(true)
-    const [jogoComecou, setJogoComecou] = useState<boolean>(false)
     const [vezDoJogador, setVezDoJogador] = useState<boolean>(true)
-
-    const selecionouCampo = (item: number) => {
-        if (tabuleiro[item]) return// Se o campo já estiver ocupado, não faz nada
-        const novoTabuleiro = [...tabuleiro]// Cria uma cópia do tabuleiro
-        novoTabuleiro[item] = 'X'// Define o símbolo X ou O dependendo do turno
-        setTabuleiro(novoTabuleiro)// Atualiza o estado do tabuleiro
-        setTurnoX(!turnoX)// Alterna o turno
-    }
-
+    const [isJogadorX, setIsJogadorX] = useState<boolean>(true)
 
     const MicheleIA = () => {//inteligencia que joga contra o player
         const novoTabuleiro = [...tabuleiro];
 
         //1. verifica se pode ganhar o jogo com uma jogada. Se sim, ela faz essa jogada.
-        const jogadapraganhar = encontrarJogadaParaGanhar(novoTabuleiro, 'O');
+        const jogadapraganhar = encontrarJogadaParaGanhar(novoTabuleiro, (isJogadorX ? 'O' : 'X'));
         if (jogadapraganhar !== null) {
-            console.log('jogada para ganhar',jogadapraganhar)
-            setVezDoJogador(true)
+            console.log('jogada para ganhar', jogadapraganhar)
             setTimeout(() => {
                 selecionaCampo(jogadapraganhar)
+                setVezDoJogador(true)
             }, 300)
             return
         }
 
         // 2. Bloquear o jogador
-        const jogadaParaBloquear = encontrarJogadaParaGanhar(novoTabuleiro, 'X');
+        const jogadaParaBloquear = encontrarJogadaParaGanhar(novoTabuleiro, (!isJogadorX ? 'O' : 'X'));
         if (jogadaParaBloquear !== null) {
-            console.log('jogada para bloquear',jogadaParaBloquear)
-            setVezDoJogador(true)
+            console.log('jogada para bloquear', jogadaParaBloquear)
             setTimeout(() => {
                 selecionaCampo(jogadaParaBloquear)
+                setVezDoJogador(true)
             }, 300)
             return
         }
@@ -43,10 +34,10 @@ export default function Home() {
         // 3. Escolher um campo estratégico (centro, cantos, etc.)
         const jogadaEstrategica = encontrarJogadaEstrategica(novoTabuleiro);
         if (jogadaEstrategica !== null) {
-            console.log('jogada estrategica',jogadaEstrategica)
-            setVezDoJogador(true)
+            console.log('jogada estrategica', jogadaEstrategica)
             setTimeout(() => {
                 selecionaCampo(jogadaEstrategica)
+                setVezDoJogador(true)
             }, 300)
             return
         }
@@ -90,49 +81,119 @@ export default function Home() {
             MicheleIA()
             console.log('alo')
         }
+        setTimeout(() => {
+            verificaGanhador()
+        }, 200)
     }, [tabuleiro])
+
+    useEffect(() => {
+        if (isJogadorX) {
+            setTurnoX(true)
+        } else {
+            setTurnoX(false)
+            MicheleIA()
+        }
+    }, [isJogadorX])
 
     const selecionaCampo = (campo: number) => {
         const novoTabuleiro = [...tabuleiro]
 
-        novoTabuleiro[campo] = 'O'
+        novoTabuleiro[campo] = (isJogadorX ? 'O' : 'X')
 
         setTabuleiro(novoTabuleiro)
+        setVezDoJogador(true)
+    }
+
+    const selecionaCampoPlayer = (campo: number) => {
+        const novoTabuleiro = [...tabuleiro]
+
+        novoTabuleiro[campo] = (isJogadorX ? 'X' : 'O')
+
+        setTabuleiro(novoTabuleiro)
+
+        setVezDoJogador(false)
+    }
+
+    const verificaGanhador = () => {
+        const linhas = [
+            [0, 1, 2], // Primeira linha
+            [3, 4, 5], // Segunda linha
+            [6, 7, 8], // Terceira linha
+            [0, 3, 6], // Primeira coluna
+            [1, 4, 7], // Segunda coluna
+            [2, 5, 8], // Terceira coluna
+            [0, 4, 8], // Diagonal principal
+            [2, 4, 6], // Diagonal secundária
+        ]
+
+        // Verifica se há algum vencedor
+        for (let i = 0; i < linhas.length; i++) {
+            const [a, b, c] = linhas[i]
+            console.log(tabuleiro[a], tabuleiro[b], tabuleiro[c])
+            if (tabuleiro[a] && tabuleiro[a] === tabuleiro[b] && tabuleiro[a] === tabuleiro[c]) {
+                console.log(a, b, c)
+                // Se todos os campos da linha têm o mesmo valor, alguém venceu
+                alert(`O jogador ${tabuleiro[a]} venceu!`)
+                return
+            }
+        }
     }
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <button onClick={() => {
-                // selecionaCampo
-            }}
-                className="mr-2 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded shadow-lg hover:shadow-xl transition duration-300 ease-in-out">
-                teste
-            </button>
-            <h1 className="text-3xl font-bold mb-4">Jogo da Velha</h1>
-            <div className="grid grid-cols-3 gap-2 bg-orange-600 rounded-md p-4">
-                {
-                    tabuleiro.map((_, index) => (
-                        <div onClick={() => {
-                            console.log(_, index)
-                            selecionouCampo(index)
-                            setVezDoJogador(false)
-                        }}
-                            className="flex items-center justify-center border rounded-md w-20 h-20 bg-white item hover:cursor-pointer">
-                            <p className={`text-5xl  ${_ === 'X' ? 'text-blue-600' : 'text-red-600'}`}>
-                                {_}
+        <main className="flex items-center justify-center">
+            <div>
+                <button onClick={() => {
+                    console.log(isJogadorX)
+                    setIsJogadorX(!isJogadorX)
+                    // selecionaCampo
+                }}
+                    className={`mr-2 ${isJogadorX ? 'bg-green-500 hover:bg-green-700' : 'bg-blue-500 hover:bg-blue-700'}  text-white font-bold py-2 px-4 rounded shadow-lg hover:shadow-xl transition duration-300 ease-in-out`}>
+                    {
+                        isJogadorX ?
+                            <p>
+                                jogar com O
                             </p>
-                        </div>
-                    ))
-                }
+                            :
+                            <p>
+                                jogar com X
+                            </p>
+                    }
+                </button>
             </div>
-            <button onClick={() => {
-                setJogoComecou(false)
-                setTurnoX(true)
-                setTabuleiro(Array(9).fill(null))
-            }}
-                className="mt-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded shadow-lg hover:shadow-xl transition duration-300 ease-in-out">
-                Reiniciar
-            </button>
-        </div>
+            <div className="flex flex-col items-center justify-center h-screen">
+                <p>O "X" começa</p>
+                <h1 className="text-3xl font-bold mb-4">Jogo da Velha</h1>
+                <div className="grid grid-cols-3 gap-2 bg-orange-600 rounded-md p-4">
+                    {
+                        tabuleiro.map((_, index) => (
+                            <div onClick={() => {
+                                console.log(_, index)
+                                selecionaCampoPlayer(index)
+                                setVezDoJogador(false)
+                            }}
+                                className="flex items-center justify-center border rounded-md w-20 h-20 bg-white item hover:cursor-pointer">
+                                <p className={`text-5xl  ${_ === 'X' ? 'text-blue-600' : 'text-red-600'}`}>
+                                    {_}
+                                </p>
+                            </div>
+                        ))
+                    }
+                </div>
+                <button onClick={() => {
+                    // setJogoComecou(false)
+                    setTurnoX(true)
+                    setTabuleiro(Array(9).fill(null))
+
+                    if (isJogadorX) {
+                        setVezDoJogador(true)
+                    } else {
+                        setVezDoJogador(false)
+                    }
+                }}
+                    className="mt-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded shadow-lg hover:shadow-xl transition duration-300 ease-in-out">
+                    Reiniciar
+                </button>
+            </div>
+        </main>
     )
 }
